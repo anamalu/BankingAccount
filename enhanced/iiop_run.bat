c040e0be8e0cdd4cdb8b5f41dfbb2e9e7d581077
anamalu <apollosmedia@gmail.com>
2012-10-18 15:33:15 -0400

    getting rid of crap

--- enhanced/iiop_run.bat
+++
@@ -1,4 +0,0 @@
-start C:\java\jdk1.3\bin\tnameserv -ORBInitialPort 1000
-start C:\java\jdk1.3\bin\java -classpath .;C:\PEJ\lib\jdo.jar;C:\PEJ\lib\j2ee.jar;C:\PEJ\lib\pej.jar; com.hywy.samples.remote.iiop.TellerServer
-pause
-C:\java\jdk1.3\bin\java -classpath .;C:\PEJ\lib\jdo.jar;C:\PEJ\lib\j2ee.jar;C:\PEJ\lib\pej.jar; com.hywy.samples.remote.client.IIOPClient
